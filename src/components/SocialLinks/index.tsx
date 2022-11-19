68a7dc114abf6ac2f798711313cd40130394a5a0
matheustakasaki <matheus8antunes@gmail.com>
2022-11-19 11:48:53 -0300

    Changing footer icons

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -1,38 +1,22 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons'
+import EmailIcon from '@mui/icons-material/Email';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 export default function SocialLinks() {
-    return (
+  return (
+    <div className='flex'>
+      <a href='https://github.com/matheustakasaki' target='_blank'>
+        <GitHubIcon />
+      </a>
+      <a
+        href='https://www.linkedin.com/in/matheus-takasaki-antunes/'
+        target='_blank'
+      >
+        <LinkedInIcon />
+      </a>
 
-
-        <div className='flex flex-col '>
-
-            <a href="https://github.com/matheustakasaki" target={'_blank'}>
-
-                <span className='pr-2'>
-
-                    Github
-                </span>
-
-                <FontAwesomeIcon icon={faArrowAltCircleRight} />
-            </a>
-            <a href="https://www.linkedin.com/in/matheus-takasaki-antunes/" target={'_blank'}>
-
-                <span className="pr-2">
-                    Linkedin
-
-                </span>
-
-                <FontAwesomeIcon icon={faArrowAltCircleRight} />
-            </a>
-
-            <a href="mailto:matheus8antunes@gmail.com" target={'_blank'}>
-
-                <span className="pr-2">
-
-                    E-mail
-                </span>
-                <FontAwesomeIcon icon={faArrowAltCircleRight} />
-            </a>
-        </div>
-    )
+      <a href='mailto:matheus8antunes@gmail.com' target='_blank'>
+        <EmailIcon />
+      </a>
+    </div>
+  );
 }
